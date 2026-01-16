15aa2ffdb46859e0c4530b352f92e5ec28d62a08
João pedro <joaopedroservico1@gmail.com>
2026-01-16 16:06:24 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// Ativa modo JS
+document.documentElement.classList.add('js');
+
+function revealOnScroll() {
+    const reveals = document.querySelectorAll('.reveal');
+
+    reveals.forEach(element => {
+        const windowHeight = window.innerHeight;
+        const elementTop = element.getBoundingClientRect().top;
+        const revealPoint = 100;
+
+        if (elementTop < windowHeight - revealPoint) {
+            element.classList.add('active');
+        }
+    });
+}
+
+window.addEventListener('scroll', revealOnScroll);
+window.addEventListener('load', revealOnScroll);
